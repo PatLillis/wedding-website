f4fbd9873a7841f76285c4671250c623681b2129
Pat Lillis <lillispm@gmail.com>
2014-10-24 14:39:47 -0400

    - fix background images on OurStory.

--- js/ourstory.js
+++ js/ourstory.js
@@ -1,13 +1,13 @@
 ﻿$(document).on('ready', function () {
-    $('a[href="#proposal"]').on('shown.bs.tab', function (e) {
-        $('.bg-img-wrapper img').removeClass('in').filter('.proposal').addClass('in');
+    $('a[href="#proposal"]').on('show.bs.tab', function (e) {
+        $('.bg-img-switch').removeClass('in').filter('.proposal').addClass('in');
     })
 
-    $('a[href="#dating"]').on('shown.bs.tab', function (e) {
-        $('.bg-img-wrapper img').removeClass('in').filter('.dating').addClass('in');
+    $('a[href="#dating"]').on('show.bs.tab', function (e) {
+        $('.bg-img-switch').removeClass('in').filter('.dating').addClass('in');
     })
 
-    $('a[href="#meeting"]').on('shown.bs.tab', function (e) {
-        $('.bg-img-wrapper img').removeClass('in').filter('.meeting').addClass('in');
+    $('a[href="#meeting"]').on('show.bs.tab', function (e) {
+        $('.bg-img-switch').removeClass('in').filter('.meeting').addClass('in');
     })
 });
